cc5ad72f07c1971b0a3f49add9f91b3a453f5753
lewistec90 <gitlmcs@gmail.com>
2020-06-16 13:09:16 -0500

    me olvido dx

---
+++ bubble_sort.cpp
@@ -0,0 +1,55 @@
+# include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int * crearAleatorios(int n)
+{
+    int* numeros = new int [n];
+
+    for(int i = 0; i < n; i++)
+    {
+        numeros[i] = rand() % n*10; 
+    }
+    return numeros;
+}
+
+void bubleSort(int *& numeros, int n)
+{   
+    int temp;
+    while(n > 0)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            if(numeros[i] > numeros[i+1])
+            {
+                temp = numeros[i];
+                numeros[i] = numeros[i+1];
+                numeros[i+1] = temp;
+            }
+        }
+        n--;
+    }
+    cout << "Ordenando" << endl;
+}
+
+void imprimir(int *numeros, int tam)
+{
+    for(int i = 0; i < tam; i++)
+    {
+        cout << numeros[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n = 10;
+    int *numeros = crearAleatorios(n);
+    imprimir(numeros, n);
+    bubleSort(numeros, n);
+    imprimir(numeros, n);
+
+
+    return 0;
+}
